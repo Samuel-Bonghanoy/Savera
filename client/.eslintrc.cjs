b4e60d38ae3c0a184039ed2cb8ebe2a825c12d7c
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-01-26 15:57:49 +0800

    chore: configure husky and lint-staged pre-commit hooks

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -2,17 +2,37 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'standard-with-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'unused-imports'],
   rules: {
-    'react-refresh/only-export-components': [
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    '@typescript-eslint/no-unused-vars': 0,
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'warn',
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    'unused-imports/no-unused-vars': [
       'warn',
-      { allowConstantExport: true },
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
     ],
+    'no-console': 'warn',
   },
-}
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
