62fa3b24ce0907881dfe8657d3695bff08745f96
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-02-02 12:48:16 +0800

    chore: configure react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AppLayout from './components/AppLayout/components';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+  },
+]);
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function App() {
-  return <></>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ client/src/components/AppLayout/components/index.tsx
@@ -0,0 +1,3 @@
+export default function AppLayout(): JSX.Element {
+  return <div>index</div>;
+}
