77acdd25e2f027cfcb0da742416290a171be4fd4
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-02-09 20:59:57 +0800

    fix: change form modal to use daisyui

--- client/src/components/FormModal/components/Modal.tsx
+++ client/src/components/FormModal/components/Modal.tsx
@@ -9,70 +9,23 @@ export default function Modal({ children }: ModalProps): JSX.Element {
   return (
     <div>
       <button
-        type="button"
-        className="inline-flex items-center pt-2 pb-1 text-sm font-semibold text-black border border-transparent rounded-lg gap-x-2 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-        data-hs-overlay="#hs-slide-up-animation-modal"
+        className="text-white border-0 btn bg-main h-fit"
+        onClick={() => document.getElementById('my_modal_2').showModal()}
       >
-        <span className="flex flex-col items-center justify-center w-full pt-2 pb-1 text-center focus:text-accent hover:text-accent">
+        <div className="flex flex-col items-center justify-center pt-2 pb-1 r w-fit focus:text-accent hover:text-accent">
           <RiMenuAddLine size={'1.5em'} />
           <span className="block text-xs tab tab-home">Add Log</span>
-        </span>
-      </button>
-
-      <div
-        id="hs-slide-up-animation-modal"
-        className="hs-overlay hidden w-full h-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
-      >
-        <div className="m-3 transition-all ease-out opacity-0 hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-14 sm:max-w-lg sm:w-full sm:mx-auto">
-          <div className="flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-            <div className="flex items-center justify-between px-4 py-3 border-b dark:border-gray-700">
-              <h3 className="font-bold text-gray-800 dark:text-white">Modal title</h3>
-              <button
-                type="button"
-                className="flex items-center justify-center text-sm font-semibold text-gray-800 border border-transparent rounded-full hs-dropup-toggle w-7 h-7 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-gray-700 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                data-hs-overlay="#hs-slide-up-animation-modal"
-              >
-                <span className="sr-only">Close</span>
-                <svg
-                  className="flex-shrink-0 w-4 h-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M18 6 6 18" />
-                  <path d="m6 6 12 12" />
-                </svg>
-              </button>
-            </div>
-            <div className="p-4 overflow-y-auto">
-              <p className="mt-1 text-gray-800 dark:text-gray-400">
-                This is a wider card with supporting text below as a natural lead-in to additional content.
-              </p>
-            </div>
-            <div className="flex items-center justify-end px-4 py-3 border-t gap-x-2 dark:border-gray-700">
-              <button
-                type="button"
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-800 bg-white border border-gray-200 rounded-lg shadow-sm hs-dropup-toggle gap-x-2 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                data-hs-overlay="#hs-slide-up-animation-modal"
-              >
-                Close
-              </button>
-              <button
-                type="button"
-                className="inline-flex items-center px-3 py-2 text-sm font-semibold text-white bg-blue-600 border border-transparent rounded-lg gap-x-2 hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-              >
-                Save changes
-              </button>
-            </div>
-          </div>
         </div>
-      </div>
+      </button>
+      <dialog id="my_modal_2" className="modal">
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">Hello!</h3>
+          <p className="py-4">Press ESC key or click outside to close</p>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </div>
   );
 }
