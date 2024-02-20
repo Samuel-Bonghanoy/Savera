f2d991df0609d02c95417422ebee36a22f7c4d6c
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-02-20 23:15:22 +0800

    chore: configure db with mysql2 and drizzle

--- server/src/db/config/db.config.ts
+++ server/src/db/config/db.config.ts
@@ -1,10 +1,11 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
-const connection = await mysql.createConnection({
-  host: "host",
-  user: "user",
-  database: "database"
+const poolConnection = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 });
 
-const db = drizzle(connection);
+export const db = drizzle(poolConnection);
