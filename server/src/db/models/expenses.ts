cd8e20a45780fb7f2575eb64fad8d5c6400b37a8
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-02-25 22:19:56 +0800

    feat: create income and expense models

---
+++ server/src/db/models/expenses.ts
@@ -0,0 +1,14 @@
+import { mysqlTable, timestamp, varchar, int, float } from "drizzle-orm/mysql-core";
+import { expenseTypes } from "./expense_types";
+
+export const expenses = mysqlTable("expenses", {
+  id: int("expense_id").notNull().primaryKey().autoincrement(),
+  amount: float("amount").notNull(),
+  description: varchar("description", { length: 256 }).notNull(),
+  incomeTypeId: int("expense_type_id")
+    .notNull()
+    .autoincrement()
+    .references(() => expenseTypes.id)
+});
+
+export type expensesModel = typeof expenses.$inferSelect;
--- server/src/db/models/incomes.ts
+++ server/src/db/models/incomes.ts
@@ -1,10 +1,14 @@
 import { mysqlTable, timestamp, varchar, int, float } from "drizzle-orm/mysql-core";
+import { incomeTypes } from "./income_types";
 
 export const incomes = mysqlTable("incomes", {
   id: int("income_id").notNull().primaryKey().autoincrement(),
   amount: float("amount").notNull(),
-  description: varchar("first_name", { length: 256 }).notNull(),
-  incomeTypeId: int("income_type_id").notNull().autoincrement()
+  description: varchar("description", { length: 256 }).notNull(),
+  incomeTypeId: int("income_type_id")
+    .notNull()
+    .autoincrement()
+    .references(() => incomeTypes.id)
 });
 
-export type UserModel = typeof incomes.$inferSelect;
+export type incomeModel = typeof incomes.$inferSelect;
