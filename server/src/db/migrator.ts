2780d1bfe1772cd859ddf169376b3832c8c5e4f4
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-02-25 22:59:47 +0800

    feat: create drizzle migrator:

--- server/src/db/migrator.ts
+++ server/src/db/migrator.ts
@@ -1,7 +1,8 @@
 import { migrate } from "drizzle-orm/mysql2/migrator";
+import { drizzle } from "drizzle-orm/mysql2";
 import path from "path";
 import { poolConnection } from "./config/db.config";
 
-migrate(poolConnection, {
+migrate(drizzle(poolConnection), {
   migrationsFolder: path.resolve(".drizzle", "migrations")
-});
+}).then(() => console.log("migration success"));
